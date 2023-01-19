cf84a5cab0cce0d59710f96bddfb43ca99bfd24b
LocalBand <agvrideteam@gmail.com>
2023-01-19 22:15:17 +0200

    Counter for games

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './counterStyles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+
+    let [player1Counter, setPlayer1Counter] = useState(0)
+    let [player2Counter, setPlayer2Counter] = useState(0)
+    const setPlayersCounter = () => {
+        setPlayer1Counter((actual) => actual - 1)
+        setPlayer2Counter((actual) => actual - 1)
+    }
+    const setPlayersCounterPlus = () => {
+        setPlayer1Counter((actual) => actual + 1)
+        setPlayer2Counter((actual) => actual + 1)
+    }
+    return (
+        <div className='wrapper'>
+            <div className='mainCard'>
+                <div className='playerACard'>
+                    <div>
+                        Player A
+                    </div>
+                    <div>{player1Counter}</div>
+                    <button onClick={() => {
+                        setPlayer1Counter((actual) => actual + 1)
+                    }}>+
+                    </button>
+                    <button onClick={() => {
+                        setPlayer1Counter((actual) => actual - 1)
+                    }}>-
+                    </button>
+                    <hr/>
+                </div>
+
+                <div className='playerBCard'>
+                    <div>
+                        Player B
+                    </div>
+                    <div>{player2Counter}</div>
+                    <button onClick={() => {
+                        setPlayer2Counter((actual) => actual + 1)
+                    }}>+
+                    </button>
+                    <button onClick={() => {
+                        setPlayer2Counter((actual) => actual - 1)
+                    }}>-
+                    </button>
+                    <hr/>
+                </div>
+
+                <div className='playersScoreManagement'>
+                    <div>
+                        A & B
+                    </div>
+                    <div>
+                        <button onClick={() => {
+                            setPlayersCounter()
+                        }}>-
+                        </button>
+                        <button onClick={() => {
+                            setPlayersCounterPlus()
+                        }}>+
+                        </button>
+                    </div>
+                    <button onClick={() => {
+                        setPlayer1Counter(0)
+                        setPlayer2Counter(0)
+                    }}>RESET
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
